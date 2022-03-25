b307277d8cd106ec5c83d3623e8dec72b464be37
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-25 16:01:03 +0100

    created component Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hero">
+    <section class="jumbotron">
+      <h1>{{ msg }}</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeoJumbo",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbotron {
+  background-color: lightblue;
+  padding: 2rem;
+  width: 70%;
+  margin: 2rem auto;
+}
+</style>
